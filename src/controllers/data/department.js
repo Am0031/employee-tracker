337f99a28743ecb068c987abbc9b574bfc76fef5
Amelie Pira <amelie.pira@gmail.com>
2022-06-26 14:17:04 +0100

    added view all dep function

--- src/controllers/data/department.js
+++ src/controllers/data/department.js
@@ -1,20 +1,19 @@
 const viewAllDepartments = async (req, res) => {
-  return res.send("view all departments");
-  // try {
-  //   const [departments] = await req.db.query("SELECT * FROM departments");
+  try {
+    const [departments] = await req.db.query("SELECT * FROM departments");
 
-  //   return res.json({
-  //     success: true,
-  //     data: departments,
-  //   });
-  // } catch (error) {
-  //   console.log(`[ERROR: Failed to get departments | ${error.message}]`);
+    return res.json({
+      success: true,
+      data: departments,
+    });
+  } catch (error) {
+    console.log(`[ERROR: Failed to get departments | ${error.message}]`);
 
-  //   return res.status(500).json({
-  //     success: false,
-  //     error: "Failed to get departments",
-  //   });
-  // }
+    return res.status(500).json({
+      success: false,
+      error: "Failed to get departments",
+    });
+  }
 };
 
 const addDepartment = async (req, res) => {
