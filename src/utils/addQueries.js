fa6dfa3344cae546e3d39adc3aad13a3e7968032
Amelie Pira <amelie.pira@gmail.com>
2022-06-27 12:14:31 +0100

    added add queries function (wip)

--- src/utils/addQueries.js
+++ src/utils/addQueries.js
@@ -2,133 +2,70 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const inquirer = require("inquirer");
 const { generateChoiceList } = require("./generateChoiceList");
+const {
+  getAllDepartments,
+  getAllEmployees,
+  getAllRoles,
+} = require("./viewQueries");
 const PORT = process.env.PORT || 4000;
 const baseUrl = `http://${process.env.DB_HOST}:${PORT}`;
 
-const getAllDepartments = async () => {
-  try {
-    const response = await fetch(`${baseUrl}/api/departments`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const result = (await response.json()).data;
-      return result;
-    } else {
-      throw new Error("Failed to fetch data");
-    }
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-const getAllRoles = async () => {
-  try {
-    const response = await fetch(`${baseUrl}/api/roles`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const result = (await response.json()).data;
-      return result;
-    } else {
-      throw new Error("Failed to fetch data");
-    }
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-const getAllEmployees = async () => {
-  try {
-    const response = await fetch(`${baseUrl}/api/employees`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const result = (await response.json()).data;
-      return result;
-    } else {
-      throw new Error("Failed to fetch data");
-    }
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-const getEmployeesByDepartment = async () => {
+const addEmployee = async () => {
   try {
     const departments = await getAllDepartments();
+    const roles = await getAllRoles();
 
-    const departmentSelection = {
-      type: "list",
-      message: "Please choose a department to see its employees:",
-      name: "id",
-      choices: generateChoiceList(departments, "depName"),
-    };
-
-    const { id } = await inquirer.prompt(departmentSelection);
-
-    const response = await fetch(`${baseUrl}/api/employees/department/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      const result = (await response.json()).data;
-      return result;
+    if (!departments.length) {
+      console.log(
+        "You must add some departments before you can add roles and employees."
+      );
+    } else if (!roles.length) {
+      console.log("You must add some roles before you can add employees.");
     } else {
-      throw new Error("Failed to fetch data");
+      const { firstName, lastName, roleId } = await inquirer.prompt(
+        addEmployeeQuestions
+      );
+
+      const employees = await getAllEmployees();
+
+      if (!employees.length) {
+        const data = { firstName, lastName, roleId };
+        const response = await fetch(`${baseUrl}/api/employees`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        if (response.ok) {
+          return true;
+        } else {
+          throw new Error("Failed to add data");
+        }
+      } else {
+        const { managerId } = await inquirer.prompt(addManagerQuestions);
+        const data = { firstName, lastName, roleId, managerId };
+        const response = await fetch(`${baseUrl}/api/employees`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        if (response.ok) {
+          return true;
+        } else {
+          throw new Error("Failed to add data");
+        }
+      }
     }
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
-const getEmployeesByManager = async () => {
-  try {
-    const employees = await getAllEmployees();
+const addRole = async () => {};
 
-    const managerSelection = {
-      type: "list",
-      message: "Please choose a manager to see its employees:",
-      name: "id",
-      choices: generateManagerList(employees),
-    };
+const addDepartment = async () => {};
 
-    const { id } = await inquirer.prompt(managerSelection);
-
-    const response = await fetch(`${baseUrl}/api/employees/manager/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      const result = (await response.json()).data;
-      return result;
-    } else {
-      throw new Error("Failed to fetch data");
-    }
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-module.exports = {
-  getAllDepartments,
-  getAllEmployees,
-  getEmployeesByDepartment,
-  getAllRoles,
-  getEmployeesByManager,
-};
+module.exports = { addDepartment, addRole, addEmployee };
